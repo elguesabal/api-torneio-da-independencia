f232da99f58db7a08f933fbc0a41f98a52f19109
elguesabal <joseelguesabal@gmail.com>
2024-09-08 08:21:46 -0300

    mds umas 15 tentativas de commit pra no final ser apenas o email bloqueando o envio pela api mesmo kkkkkkkkk

--- user/feedback.js
+++ user/feedback.js
@@ -20,21 +20,12 @@ function enviarFeedback(email, feedback, res) {
 	};
 	transport.sendMail(dadosEmail)
 	.then((response) => res.status(200).send("Email enviado"))
-	.catch((error) => res.status(500).send("Falha ao enviar email"))
-	// .then((response) => console.log("Email enviado"))
-	// .catch((error) => console.log("Falha ao enviar email"))
+	.catch((error) => res.status(500).send("Falha ao enviar email"));
 }
 
 export default function feedback(app) {
 	app.post("/feedback", (req, res) => {
-		try {
-			const feedback = req.body;
-
-			enviarFeedback("joseelguesabal@gmail.com", feedback, res);
-			// setTimeout(() => enviarFeedback("xxxx@gmail.com", feedback), 5000); // COLOCAR O EMAIL DO ALEXANDRE
-			// res.status(200).send("ok");
-		} catch (error) {
-			// res.status(500).send("Erro ao salvar feedback.");
-		}
+		enviarFeedback("joseelguesabal@gmail.com", req.body, res);
+		setTimeout(() => enviarFeedback("badbonsucesso@gmail.com", req.body, res), 5000);
 	});
 }
