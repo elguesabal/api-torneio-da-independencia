db8e28876e982cd565c4d103c3adc8b862654a0e
elguesabal <joseelguesabal@gmail.com>
2024-09-08 08:16:30 -0300

    enviando o decimo terceiro log de depuracao

--- user/feedback.js
+++ user/feedback.js
@@ -27,32 +27,14 @@ function enviarFeedback(email, feedback, res) {
 
 export default function feedback(app) {
 	app.post("/feedback", (req, res) => {
-		const feedback = req.body;
+		try {
+			const feedback = req.body;
 
-		// enviarFeedback("joseelguesabal@gmail.com", feedback, res);
-		// setTimeout(() => enviarFeedback("xxxx@gmail.com", feedback), 5000); // COLOCAR O EMAIL DO ALEXANDRE
-
-
-
-		const transport = nodemailer.createTransport({
-			host: 'smtp.office365.com',
-			port: 587,
-			secure: false,
-			auth: {
-				user: process.env.EMAIL,
-				pass: process.env.SENHA
-			}
-		});
-		const dadosEmail = {
-			from: `Torneio da independencia ${process.env.EMAIL}`,
-			to: email,
-			replyTo: feedback.feedback,
-			subject: "Feedback recebido!",
-			html: `<h1>Nome: ${feedback.nome}</h1><p>Feedback: ${feedback.feedback}</p>`,
-			text: `Nome: ${feedback.nome} Feedback: ${feedback.feedback}`
-		};
-		transport.sendMail(dadosEmail)
-		.then((response) => res.status(200).send("Email enviado"))
-		.catch((error) => res.status(500).send("Falha ao enviar email: " + error))
+			enviarFeedback("joseelguesabal@gmail.com", feedback, res);
+			// setTimeout(() => enviarFeedback("xxxx@gmail.com", feedback), 5000); // COLOCAR O EMAIL DO ALEXANDRE
+			// res.status(200).send("ok");
+		} catch (error) {
+			// res.status(500).send("Erro ao salvar feedback.");
+		}
 	});
 }
